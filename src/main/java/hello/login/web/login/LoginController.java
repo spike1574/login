e098ed2068185612cf7a2a62f6fd182f41269301
spike1574 <spike1574@naver.com>
2024-01-05 14:04:10 +0900

    89강

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -133,6 +133,7 @@ public class LoginController {
 
         // 세션에 로그인 회원 정보 보관
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
         return  "redirect:" + redirectURL;
     }
 
